3cd505fa5956b447e44302d997804bc515ab83c3
Lukas K Schumann <lukas_kilian.schumann@stud-mail.uni-wuerzburg.de>
2024-01-18 17:19:17 +0100

    fix oled timeout

--- keyboards/crkbd/keymaps/lkschu/config.h
+++ keyboards/crkbd/keymaps/lkschu/config.h
@@ -37,7 +37,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define TAPPING_FORCE_HOLD
 //#define TAPPING_TERM 100
 
-#define OLED_TIMEOUT // we handle the timeout manually!
+#define OLED_TIMEOUT 0 // we handle the timeout manually!
 #define OLED_TIMEOUT_CUSTOM 300000
 #define OLED_BRIGHTNESS 70
 #define SPLIT_WPM_ENABLE
